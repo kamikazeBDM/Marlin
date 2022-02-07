470af6bc19961d3ddd6a0fab2ac89f2e583788dc
m2mwireless <72998363+m2mwireless@users.noreply.github.com>
2022-02-07 14:00:02 -0800

    new fork import
    forked from marlin github
    prepping config files from newly setup Octopus Pro v1.0 board.

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -21,6 +21,158 @@
  */
 #pragma once
 
+
+// BOARD NAME: BOARD_BTT_OCTOPUS_PRO_V1_0
+// BigTreeTech Octopus Pro V1.0 (STM32F429ZGT6 ARM Cortex-M4)
+
+
+/*************************************
+ * Marlin bugfix-2.0.x
+ * Compiled version identifier
+ ************************************/
+#define COMPILE_VERSION "03" 
+
+#define SHORT_BUILD_VERSION "bugfix-2.0.9.3-" COMPILE_VERSION
+#define STRING_CONFIG_H_AUTHOR "BDM"                    // Who made the changes.
+#define CUSTOM_MACHINE_NAME "BTT OCTOPUS PRO v1.0 TMC-2209"     // Name displayed in the LCD "Ready" message and Info menu
+#define MACHINE_NAME "JAM-ENG Custom Ender 3 Pro"
+
+/*******************************************************************************************************
+ * 
+ * Vxx MM/DD/2022 HH:MM
+ * 
+ * !! BOARD_BTT_OCTOPUS_PRO_V1_0  <-- BUILD TARGET 
+ * !! BigTreeTech Octopus Pro V1.0 (STM32F429ZGT6 ARM Cortex-M4)
+ * 
+ * V03  02/06/2022 13:12  
+ *          M92 X80.6250 Y80.6250 Z402.0000
+ *          M92 T0 E397.1500
+ *          M92 T1 E397.1500
+ *          M203 X180.0000 Y180.0000 Z12.0000
+ *          M203 T0 E200.0000
+ *          M203 T1 E200.0000
+ *          M201 X2000.0000 Y2000.0000 Z200.0000
+ *          M201 T0 E8000.0000
+ *          M201 T1 E8000.0000
+ *          M204 P750.0000 R1000.0000 T1000.0000
+ *          M205 B20000.0000 S0.0000 T0.0000 J0.0180
+ *          M218 T1 X-19.9700 Y-0.1000 Z0.000
+ *          M301 E0 P17.1585 I1.4116 D52.1403
+ *          M301 E1 P15.4397 I1.0324 D57.7252
+ *          M304 P143.7940 I28.0932 D490.6750
+ *          M906 X800 Y800 Z900
+ *          M906 T0 E580
+ *          M906 T1 E580
+ *          M913 X100 Y100 Z15
+ *          M913 T0 E26
+ *          M913 T1 E26
+ *          M603 T0 L80.0000 U125.0000 ; (mm)
+ *          M603 T1 L80.0000 U125.0000 ; (mm)
+ *          M217 S12.0000 B0.0000 E0.0000 P276.0000 R3000.0000 U1500.0000 F255 G10 A0 L0.0000 Z2.0000
+ *          M569 S1 Z
+ *          M569 S1 T0 E
+ *          M569 S1 T1 E
+ *          #define FAST_PWM_FAN
+ *            #define FAST_PWM_FAN_FREQUENCY 39200 
+ *          #define Z_AFTER_HOMING   10
+ *          #define ENCODER_PULSES_PER_STEP 4
+ *          #define SD_ABORT_NO_COOLDOWN          // Leave the heaters on after Stop Print (not recommended!)
+ *          REMOVED:   #define SD_DETECT_STATE HIGH
+ * 
+ * V02  02/05/2022 13:08  
+ * 
+ *      //////////////////////////////////////////////////////
+ *      // BIGTREE_OCTOPUS_V1   (STM32F446ZET6 ARM Cortex-M4)
+ *      // MOTHERBOARD: BOARD_BTT_OCTOPUS_PRO_V1_0
+ *      //////////////////////////////////////////////////////
+ *          
+ *      IMPORTED SETTINGS FROM BTT GTR PRO 1.0 
+ * 
+ *          //  [ TFT/UART1]  BTT OCTOPUS PRO V1.0 
+ *          //                TFT PORT TOUCH/GUI MODE
+ *          #define SERIAL_PORT 1
+ *          #define BAUDRATE 115200
+ *          #define BAUD_RATE_GCODE     // Enable G-code M575 to set the baud rate
+ * 
+ *          //  [ USART 2 ]   BTT OCTOPUS PRO V1.0 
+ *          //                RASPBERRY PI 3/4b - OCTOPRINT
+ *          #define SERIAL_PORT_2 2
+ *          #define BAUDRATE_2 115200   // Enable to override BAUDRATE
+ *          
+ *          // E0 PID = M301 E0 P17.1585 I1.4116 D52.1403
+ *          // E1 PID = M301 E1 P15.4397 I1.0324 D57.7252
+ *          // BED PID= M304 P143.7948 I28.0932 D490.6759
+ *  
+ *          #define DEFAULT_AXIS_STEPS_PER_UNIT  { 80.625, 80.625, 402.000, 354.454, 354.454 }
+ * 
+ * V01  02/04/2022 14:24  
+ *      ** INITIAL INSTALL - REPLACING BTT GTR 1.0 - Broken Board - does not boot after shorting Part Fan Controller
+ *      REPOSITORYDOWNLOAD: MARLIN upstream bugfix-2.0(.9.3)  2/3/2022
+ *          #define DISABLE_X false
+ *          #define DISABLE_Y false
+ *          #define DISABLE_Z false
+ *           #define MANUAL_FEEDRATE { 100*60, 100*60, 12*60, 2*60 } // (mm/min) Feedrates for manual moves along X, Y, Z, E from panel
+ * 
+ *******************************************************************************************************
+*/
+
+
+
+
+
+
+
+
+
+/**
+ * Verbose version identifier which should contain a reference to the location
+ * from where the binary was downloaded or the source code was compiled.
+ */
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (Github/kamikazebdm/marlin)"
+
+/**
+ * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
+ * here we define this default string as the date where the latest release
+ * version was tagged.
+ */
+#define STRING_DISTRIBUTION_DATE "2021-12-31"
+
+/**
+ * Defines a generic printer name to be output to the LCD after booting Marlin.
+ */
+//#define MACHINE_NAME "3D Printer"
+
+/**
+ * The SOURCE_CODE_URL is the location where users will find the Marlin Source
+ * Code which is installed on the device. In most cases —unless the manufacturer
+ * has a distinct Github fork— the Source Code URL should just be the main
+ * Marlin repository.
+ */
+#define SOURCE_CODE_URL "github.com/MarlinFirmware/Marlin"
+
+/**
+ * Default generic printer UUID.
+ */
+//#define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
+
+/**
+ * The WEBSITE_URL is the location where users can get more information such as
+ * documentation about a specific Marlin release.
+ */
+#define WEBSITE_URL "marlinfw.org"
+
+/**
+ * Set the vendor info the serial USB interface, if changable
+ * Currently only supported by DUE platform
+ */
+//#define  USB_DEVICE_VENDOR_ID           0x0000
+//#define  USB_DEVICE_PRODUCT_ID          0x0000
+//#define  USB_DEVICE_MANUFACTURE_NAME    WEBSITE_URL
+
+
+
+
+
 ////////////////////////////
 // VENDOR VERSION EXAMPLE //
 ////////////////////////////
@@ -41,7 +193,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2022-02-07"
+//#define STRING_DISTRIBUTION_DATE "2022-02-02"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
